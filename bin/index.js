96fe22e83da5a9c24144e7bc014bd855875fdd4c
Matthew Swanberg <matt20swanberg@gmail.com>
2026-03-18 17:41:21 -0400

    Added logic to run trivia game

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
-import { program } from "commander";
 import gameState from "../src/lib/state.js";
 import { showMainMenu } from "../src/lib/gameLogic.js";
+import questions from "../src/lib/questions.js";
+
+async function run() {
+  await showMainMenu(gameState);
+}
+
+run();
